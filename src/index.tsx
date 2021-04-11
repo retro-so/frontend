ffbdbffba70672a8454af51c262eee547cdcd5d0
yarastqt <yarastqt@gmail.com>
2021-04-11 04:01:13 +0300

    feat: use react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Router } from 'react-router'
 import { render } from 'react-dom'
 import { configureRootTheme } from '@yandex/ui/Theme'
 import { theme } from '@yandex/ui/Theme/presets/default'
 
-import { App } from './App'
+import { Pages } from './pages'
 import reportWebVitals from './reportWebVitals'
+import { history } from './libs/history'
+import './libs/firebase'
 import './index.css'
 
 configureRootTheme({ theme })
 
-render(<App />, document.getElementById('root'))
+render(
+  <Router history={history}>
+    <Suspense fallback="Loading...">
+      <Pages />
+    </Suspense>
+  </Router>,
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
