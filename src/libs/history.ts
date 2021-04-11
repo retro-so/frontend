0323c4737b713f93211a0ad12d8fc8e336758e84
yarastqt <yarastqt@gmail.com>
2021-04-11 21:47:12 +0300

    feat: impl pushHistoryFx for history

--- src/libs/history.ts
+++ src/libs/history.ts
@@ -1,3 +1,6 @@
+import { createEffect } from 'effector'
 import { createBrowserHistory } from 'history'
 
 export const history = createBrowserHistory()
+
+export const pushHistoryFx = createEffect((path: string) => history.push(path))
