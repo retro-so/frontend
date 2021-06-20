71906421b3933c74e65a523e59764de51227c0a8
yarastqt <yarastqt@gmail.com>
2021-06-20 19:51:54 +0300

    feat(client): use bg for login page

--- packages/client/src/pages/LoginPage/LoginPage.tsx
+++ packages/client/src/pages/LoginPage/LoginPage.tsx
@@ -1,29 +1,59 @@
 import { FC } from 'react'
 import { Button } from '@yandex/ui/Button/desktop/bundle'
 import { Text } from '@yandex/ui/Text/bundle'
+import styled from '@emotion/styled'
 
 import { Flex } from '../../components/Flex'
 import { useAnonymousGuard } from '../../features/session'
+import background from './background.svg'
 
 export const LoginPage: FC = () => {
   useAnonymousGuard()
 
   return (
-    <Flex direction="column" justifyContent="center" alignItems="center" gap="32px" height="100vh">
-      <Text typography="headline-l" weight="medium">
-        Welcome to retroboard
-      </Text>
-      <Text typography="subheader-l" weight="medium">
-        Login via
-      </Text>
-      <Flex gap="16px">
-        <Button view="action" size="m" type="link" url="http://localhost:3100/api/v1/auth/yandex">
-          Yandex
-        </Button>
-        <Button disabled view="pseudo" size="m" onClick={() => null}>
-          Anonymous
-        </Button>
-      </Flex>
-    </Flex>
+    <Container>
+      <Side>
+        <Text typography="headline-l" weight="medium">
+          Welcome to retroboard
+        </Text>
+        <Text typography="subheader-l" weight="medium">
+          Login via
+        </Text>
+        <Flex gap="16px">
+          <Button view="pseudo" size="l" type="link" url="http://localhost:3100/api/v1/auth/yandex">
+            Yandex
+          </Button>
+          <Button view="pseudo" size="l" onClick={() => null}>
+            Anonymous
+          </Button>
+        </Flex>
+      </Side>
+      <Side>
+        <Placeholder />
+      </Side>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`
+
+const Side = styled.div`
+  flex: 0 0 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+`
+
+const Placeholder = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(${background});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`
